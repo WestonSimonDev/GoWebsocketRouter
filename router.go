00fe826eaea3f7a09b8c19259c9a853832cf4dc0
WestonSimonDev <weston@wcloud.dev>
2024-11-30 02:51:25 +0000

    Added top level router handel request function.

--- router.go
+++ router.go
@@ -91,3 +91,61 @@ func (router *RouteRegistration) HandleRequest(request Request, httpRequest *htt
 	}
 	return responseBytes, nil
 }
+
+func (router *TopLevelRouteRegistration) HandleRequest(request Request, httpRequest *http.Request) ([]byte, error) {
+
+	parsedPath := parsePath(request.Path)
+
+	request.Path = parsedPath[len(parsedPath)-1]
+
+	if len(parsedPath) == 1 {
+		endpoint, ok := router.EndPoints[parsedPath[0]]
+		if ok {
+
+			responseChan := make(chan []byte)
+			errChan := make(chan error)
+
+			go asyncEndpointRunner(endpoint, request.Payload, responseChan, errChan, httpRequest)
+
+			response := <-responseChan
+			err := <-errChan
+			responseBytes, jsonErr := json.Marshal(Response{Path: fmt.Sprintf("response/%s", request.Response), Payload: response})
+			if jsonErr != nil {
+				return nil, jsonErr
+			}
+			return responseBytes, err
+
+		} else {
+			wildCardEndpoint, wildOk := router.EndPoints["*"]
+			if wildOk {
+				responseChan := make(chan []byte)
+				errChan := make(chan error)
+
+				go asyncEndpointRunner(wildCardEndpoint, request.Payload, responseChan, errChan, httpRequest)
+
+				response := <-responseChan
+				err := <-errChan
+				responseBytes, jsonErr := json.Marshal(Response{Path: fmt.Sprintf("response/%s", request.Response), Payload: response})
+				if jsonErr != nil {
+					return nil, jsonErr
+				}
+				return responseBytes, err
+			}
+		}
+	} else {
+		subRouter, ok := router.SubRouters[parsedPath[0]]
+		if ok {
+			return subRouter.HandleRequest(request, httpRequest)
+		}
+
+	}
+	var pathNotFoundResponse = make(map[string]string)
+	pathNotFoundResponse["error"] = "Action Not Found"
+	payload, _ := json.Marshal(pathNotFoundResponse)
+
+	responseBytes, jsonErr := json.Marshal(Response{Path: fmt.Sprintf("response/%s", request.Response), Payload: payload})
+	if jsonErr != nil {
+		return nil, jsonErr
+	}
+	return responseBytes, nil
+}
